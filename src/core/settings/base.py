c194b5249df27225c4cf2f82bd36117b64f989c1
Dalila <dalilamv99@outlook.com>
2022-07-06 20:37:39 -0300

    fix admin

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.sites',
+    # 'django.contrib.sites',
 
     # LIBS
     # 'nested_admin',
